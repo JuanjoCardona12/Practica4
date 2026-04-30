e78b50910fc47477aa5632bc1919ee0fcb6f855a
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-30 11:02:48 -0500

    Cambios en red.h

--- Red.h
+++ Red.h
@@ -1,1 +1,23 @@
 
+#ifndef RED_H
+#define RED_H
+
+#include "Enrutador.h"
+#include <map>
+#include <string>
+#include <list> // NECESARIO PARA std::list
+
+class Red {
+private:
+    std::map<std::string, Enrutador*> enrutadores;
+
+public:
+    ~Red();
+    void agregarEnrutador(std::string nombre);
+    void conectar(std::string origen, std::string destino, int costo);
+    void desconectar(std::string n1, std::string n2);
+    void cargarDesdeArchivo(std::string nombreArchivo);
+    void calcularMejorRuta(std::string origen, std::string destino);
+};
+
+#endif
