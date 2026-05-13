44a5044879532cbd9d81e45763ea76721debdce4
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-05-13 13:20:38 -0500

    Update Red.h

--- Red.h
+++ Red.h
@@ -1,23 +1,37 @@
-
 #ifndef RED_H
 #define RED_H
 
 #include "Enrutador.h"
 #include <map>
 #include <string>
-#include <list>
 
 class Red {
+
 private:
+
     std::map<std::string, Enrutador*> enrutadores;
 
 public:
+
     ~Red();
+
+    // Gestion de routers
     void agregarEnrutador(std::string nombre);
+    void eliminarEnrutador(std::string nombre);
+
+    // Gestion de enlaces
     void conectar(std::string origen, std::string destino, int costo);
     void desconectar(std::string n1, std::string n2);
+
+    // Archivo
     void cargarDesdeArchivo(std::string nombreArchivo);
+
+    // Algoritmo de rutas
     void calcularMejorRuta(std::string origen, std::string destino);
+
+    // Mostrar informacion
+    void mostrarRed();
+    void mostrarTablaCostos();
 };
 
 #endif
